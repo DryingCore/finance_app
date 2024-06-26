59fee16cb975dac299b836d75617020d8cefb941
DryingCore <lein0rb4g@gmail.com>
2024-06-26 20:28:36 +0000

    Jun 26, 2024, 5:28 PM

--- src/pages/MainInterface.tsx
+++ src/pages/MainInterface.tsx
@@ -4,7 +4,7 @@ import TranscationCard from './Transaction';
 
 export default function MainInterface() {
     return (
-        <Box sx={{ flexGrow: 1, padding: 0, backgroundColor: 'red', height: '50%', width: '60%' }}>
+        <Box sx={{ flexGrow: 1, padding: 0, height: '50%', width: '60%' }}>
             <Grid sx={{ justifyContent: 'end', alignItems: 'center', width: '100%' }}>
                 <Grid xs={12} md={8}>
                     <TranscationCard />
