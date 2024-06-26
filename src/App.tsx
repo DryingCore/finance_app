c55d80515cad968b53bc9d57864fe57121d4d854
DryingCore <lein0rb4g@gmail.com>
2024-06-26 19:35:06 +0000

    Jun 26, 2024, 4:35 PM

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Container } from '@mui/material'
+import ButtonsLeft from './pages/ButtonsLeft.tsx'
+import MainInterface from './pages/MainInterface.tsx'
 import './styles/main.css'
-import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
-
-    <Container sx={{ backgroundColor: 'black', height: '100vh' }}>
-      <Dashboard />
-    </Container>
+    <Container sx={{ backgroundColor: 'black', height: '100vh', display: 'flex' }}>
+      <ButtonsLeft />
+      <MainInterface />
+    </Container >
   )
 }
 
