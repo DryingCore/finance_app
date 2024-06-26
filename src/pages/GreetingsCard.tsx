b9c1566d7d14db0d20d16c832d6acb23276b173d
DryingCore <lein0rb4g@gmail.com>
2024-06-26 22:02:16 +0000

    Jun 26, 2024, 7:02 PM

--- src/pages/GreetingsCard.tsx
+++ src/pages/GreetingsCard.tsx
@@ -3,16 +3,16 @@ import { Card, CardContent, Typography } from "@mui/material";
 export default function GreetingsCard() {
     return (
         <>
-                <Card sx={{ minWidth: 275 }}>
-                    <CardContent>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Welcome Back
-                        </Typography>
-                        <Typography variant="h5" component="div">
-                            John Doe
-                        </Typography>
-                    </CardContent>
-                </Card>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Welcome Back
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        John Doe
+                    </Typography>
+                </CardContent>
+            </Card>
         </>
     )
 }
