753b197264c6b83d4d550c802bbcd2c0f6e4eb3d
DryingCore <lein0rb4g@gmail.com>
2024-06-26 22:24:08 +0000

    Jun 26, 2024, 7:24 PM

--- src/pages/MainInterface.tsx
+++ src/pages/MainInterface.tsx
@@ -6,7 +6,7 @@ import GreetingsCard from './GreetingsCard';
 export default function MainInterface() {
     return (
         <Box sx={{ flexGrow: 1, padding: 0, height: '50%', width: '60%', marginLeft: '20px' }}>
-            <Grid container justifyContent="end" alignItems="center" rowSpacing={2} width="100%">
+            <Grid container justifyContent="end" alignItems="center" rowSpacing={2.3} width="100%">
                 <Grid>
                     <GreetingsCard />
                 </Grid>
