a7e8301e21834b41ccf6e44e28bcdbfe3e025b35
DryingCore <lein0rb4g@gmail.com>
2024-06-26 19:56:24 +0000

    Jun 26, 2024, 4:56 PM

--- src/pages/MainInterface.tsx
+++ src/pages/MainInterface.tsx
@@ -1,21 +1,8 @@
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import TranscationCard from './Transaction';
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.background.paper,
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.primary,
-    boxShadow: 'none',
-    border: `1px solid ${theme.palette.divider}`,
-    borderRadius: theme.shape.borderRadius,
-}));
-
-function MainInterface() {
+export default function MainInterface() {
     return (
         <Box sx={{ flexGrow: 1, padding: 0, backgroundColor: 'red', height: '50%', width: '60%' }}>
             <Grid sx={{ justifyContent: 'end', alignItems: 'center', width: '100%' }}>
@@ -27,5 +14,4 @@ function MainInterface() {
     );
 }
 
-export default MainInterface
 
