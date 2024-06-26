294ff7c82408df87aa58f44510d30e1e5b0e307e
DryingCore <lein0rb4g@gmail.com>
2024-06-26 19:54:03 +0000

    Jun 26, 2024, 4:54 PM

---
+++ src/pages/Transaction.tsx
@@ -0,0 +1,18 @@
+import { Card, CardContent, Typography } from "@mui/material";
+
+export default function BalanceCard() {
+    return (
+        <>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Type: Expense
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        Value: R$1600,00
+                    </Typography>
+                </CardContent>
+            </Card>
+        </>
+    )
+}
