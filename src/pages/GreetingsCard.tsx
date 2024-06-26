116cde8f15eca18325ecc6ad97f4a162203fae62
DryingCore <lein0rb4g@gmail.com>
2024-06-26 22:00:26 +0000

    Jun 26, 2024, 7:00 PM

---
+++ src/pages/GreetingsCard.tsx
@@ -0,0 +1,20 @@
+import { Card, CardContent, Typography } from "@mui/material";
+
+export default function GreetingsCard() {
+    return (
+        <>
+            <>
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            Total Balance
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                            R$1600,00
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </>
+        </>
+    )
+}
--- src/pages/MainInterface.tsx
+++ src/pages/MainInterface.tsx
@@ -1,13 +1,14 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import TransactionCard from './Transaction';
+import GreetingsCard from './GreetingsCard';
 
 export default function MainInterface() {
     return (
         <Box sx={{ flexGrow: 1, padding: 0, height: '50%', width: '60%', marginLeft: '20px' }}>
             <Grid container justifyContent="end" alignItems="center" rowSpacing={2} width="100%">
                 <Grid>
-                    <TransactionCard />
+                    <GreetingsCard/>
                 </Grid>
                 <Grid xs={12}>
                     <TransactionCard />
