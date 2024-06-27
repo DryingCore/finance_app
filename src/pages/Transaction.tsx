48a76451622b14a1bacce7ed2b20cd41ab8a46b7
DryingCore <lein0rb4g@gmail.com>
2024-06-27 23:02:10 +0000

    Jun 27, 2024, 8:02 PM

--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,18 +1,21 @@
+import React from 'react';
 import { Card, CardContent, Typography } from "@mui/material";
 
-export default function TranscationCard({ value }) {
+interface TransactionCardProps {
+    value: number;
+}
+
+export default function TranscationCard({ value }: TransactionCardProps) {
     return (
-        <>
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        Type: Expense
-                    </Typography>
-                    <Typography variant="h5" component="div">
-                        Value: R${value}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </>
-    )
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Type: Expense
+                </Typography>
+                <Typography variant="h5" component="div">
+                    Value: R${value}
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 }
