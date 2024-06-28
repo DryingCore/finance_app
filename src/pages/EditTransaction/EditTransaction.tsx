74cb8c45b8db3a23a1ac364c3835b2af1da702b2
DryingCore <lein0rb4g@gmail.com>
2024-06-28 23:46:21 +0000

    Jun 28, 2024, 8:46 PM

--- src/pages/EditTransaction/EditTransaction.tsx
+++ src/pages/EditTransaction/EditTransaction.tsx
@@ -1,21 +1,60 @@
-import { Card, CardContent, Typography } from "@mui/material";
-export default function EditTransaction() {
+import { styled } from '@mui/material/styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faDollarSign, faEdit, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2'; // Or '@mui/material/Grid' if using stable Grid
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.background.paper,
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.primary,
+    boxShadow: 'none',
+    border: `1px solid ${theme.palette.divider}`,
+    borderRadius: theme.shape.borderRadius,
+}));
+
+interface ButtonsLeftProps {
+    onEdit: () => void;
+}
+
+export default function EditTransaction({ onEdit }: ButtonsLeftProps) {
     return (
-        <>
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        <span>
-                            Which you want to edit?
-                        </span>
-                    </Typography>
-                    <Typography variant="h5" component="div">
-                        <span>
-                            R$1600,00
-                        </span>
-                    </Typography>
-                </CardContent>
-            </Card>
-        </>
-    )
+        <Box sx={{ flexGrow: 1, padding: 0, height: '50%', width: '20%' }}>
+            <Grid container rowSpacing={2}>
+                <Grid xs={12}>
+                    <Item>
+                        <FontAwesomeIcon icon={faDollarSign} className='mr-[10px]' size='lg' />
+                        <span>Your transactions</span>
+                    </Item>
+                </Grid>
+                <Grid xs={12}>
+                    <Item>
+                        <FontAwesomeIcon icon={faBell} className='mr-[10px]' size='lg' />
+                        <span>Last updates</span>
+                    </Item>
+                </Grid>
+                <Grid xs={12}>
+                    <Item>
+                        <FontAwesomeIcon icon={faPlus} className='mr-[10px]' size='lg' />
+                        <span>Add new transaction</span>
+                    </Item>
+                </Grid>
+                <Grid xs={12}>
+                    <Item>
+                        <FontAwesomeIcon icon={faMinus} className='mr-[10px]' size='lg' />
+                        <span>Remove transaction</span>
+                    </Item>
+                </Grid>
+                <Grid xs={12}>
+                    <Item onClick={onEdit}>
+                        <FontAwesomeIcon icon={faEdit} className='mr-[10px]' size='lg' />
+                        <span>Edit transaction</span>
+                    </Item>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
