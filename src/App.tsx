74d4beeba53eeccf061599a711e96b880f4aa5bb
DryingCore <lein0rb4g@gmail.com>
2024-06-30 02:00:27 +0000

    Jun 29, 2024, 11:00 PM

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { AuthProvider, useAuth } from './AuthContext';
+import { AuthProvider, useAuth } from './context/AuthContext';
 import LoginForm from './pages/Login/LoginForm.tsx'; // Importe o componente LoginForm correto
 import Dashboard from './pages/MainApplication.tsx'; // Importe o componente Dashboard correto
-import PrivateRoute from './PrivateRoute';
+import PrivateRoute from './src/PrivateRoute';
 import './styles/main.css';
 
 const App: React.FC = () => {
