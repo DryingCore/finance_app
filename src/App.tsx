f995f3a6bdd5ae22c8ee8af75cad9e751212d4ba
DryingCore <lein0rb4g@gmail.com>
2024-06-30 00:06:14 +0000

    Jun 29, 2024, 9:06 PM

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { Container } from '@mui/material';
 import ButtonsLeft from './pages/Dashboard/ButtonsLeft.tsx';
 import MainInterface from './pages/Dashboard/MainInterface.tsx';
 import './styles/main.css';
+import LabelBottomNavigation from './pages/Dashboard/Teste.tsx';
 
 function App() {
 
   return (
     <Container sx={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {/* 
       <ButtonsLeft />
       <MainInterface />
+      */}
+      <LabelBottomNavigation/>
     </Container>
   );
 }
