2f73931b0f06dd979ec5555c95d004e4bb2f3f33
DryingCore <lein0rb4g@gmail.com>
2024-06-30 02:02:12 +0000

    Jun 29, 2024, 11:02 PM

--- src/pages/MainApplication.tsx
+++ src/pages/MainApplication.tsx
@@ -1,15 +1,13 @@
+import React from 'react';
 import { Container } from '@mui/material';
 import ButtonsLeft from './Dashboard/ButtonsLeft.tsx';
 import MainInterface from './Dashboard/MainInterface.tsx';
-import './styles/main.css';
-
 
 export default function MainApplication() {
-
     return (
         <Container sx={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <ButtonsLeft />
-            <MainInterface
+            <MainInterface />
         </Container>
     );
 }
